1a4fad64d8c8db9dc93654ad9287140aabbe3213
yaochong-cc <147566535+yaochong-cc@users.noreply.github.com>
2024-04-07 17:09:12 +0800

    Add files via upload

--- 平衡二叉树.cpp
+++ 平衡二叉树.cpp
@@ -9,22 +9,68 @@ struct TreeNode {
 	TreeNode* right;
 	TreeNode(int val) :val(val), left(NULL), right(NULL) {};
 };
-class Solution {//递归法
+//class Solution {//递归法
+//public:
+//	int getDepth(TreeNode* node) {
+//		if (node == NULL) return 0;
+//		int leftDepth = getDepth(node->left);
+//		if (leftDepth == -1) return -1;
+//		int rightDepth = getDepth(node->right);
+//		if (rightDepth == -1) return -1;
+//		return abs(leftDepth - rightDepth) > 1 ? -1 : 1 + max(leftDepth, rightDepth);
+//	}
+//	bool isBalance(TreeNode* root) {
+//		return getDepth(root) == -1 ? false : true;
+//	}
+//
+//};
+
+class Solution {
+private:
+    int getDepth(TreeNode* cur) {
+        stack<TreeNode*> st;
+        if (cur != NULL) st.push(cur);
+        int depth = 0; // 记录深度
+        int result = 0;
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            if (node != NULL) {
+                st.pop();
+                st.push(node);                          // 中
+                st.push(NULL);
+                depth++;
+                if (node->right) st.push(node->right);  // 右
+                if (node->left) st.push(node->left);    // 左
+
+            }
+            else {
+                st.pop();
+                node = st.top();
+                st.pop();
+                depth--;
+            }
+            result = result > depth ? result : depth;
+        }
+        return result;
+    }
+
 public:
-	int getDepth(TreeNode* node) {
-		if (node == NULL) return 0;
-		int leftDepth = getDepth(node->left);
-		if (leftDepth == -1) return -1;
-		int rightDepth = getDepth(node->right);
-		if (rightDepth == -1) return -1;
-		return abs(leftDepth - rightDepth) > 1 ? -1 : 1 + max(leftDepth, rightDepth);
-	}
-	bool isBalance(TreeNode* root) {
-		return getDepth(root) == -1 ? false : true;
-	}
-
+    bool isBalanced(TreeNode* root) {
+        stack<TreeNode*> st;
+        if (root == NULL) return true;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();                       // 中
+            st.pop();
+            if (abs(getDepth(node->left) - getDepth(node->right)) > 1) {
+                return false;
+            }
+            if (node->right) st.push(node->right);           // 右（空节点不入栈）
+            if (node->left) st.push(node->left);             // 左（空节点不入栈）
+        }
+        return true;
+    }
 };
-
 
 int main() {
 	Solution s;
@@ -35,6 +81,8 @@ int main() {
 	root->left->right = new TreeNode(5);
 	root->right->left = new TreeNode(6);
 	root->right->right = new TreeNode(7);
-	cout << s.isBalance(root) << endl;	
+	cout << s.isBalanced(root) << endl;	
 	return  0;
 }
+
+

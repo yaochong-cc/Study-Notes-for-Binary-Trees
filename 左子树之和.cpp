7725f263aa19a98df1c650fba51ac017fe19e6f8
yaochong-cc <147566535+yaochong-cc@users.noreply.github.com>
2024-04-08 22:13:54 +0800

    Add files via upload

---
+++ 左子树之和.cpp
@@ -0,0 +1,56 @@
+//#include<iostream>
+//#include<queue>
+//#include<stack>
+//using namespace std;
+//struct TreeNode {
+//    int val;
+//    TreeNode* left;
+//    TreeNode* right;
+//    TreeNode(int x) :val(x), left(NULL), right(NULL) {};
+//};
+//////递归法
+////class Solution{
+////public:
+////    int sumOfLeftLeaves(TreeNode* root) {
+////        if (root == NULL)return 0;
+////        if (root->left == NULL && root->right == NULL) return 0;
+////        int leftNum = sumOfLeftLeaves(root->left);//左
+////        if (root->left && !root->left->left && !root->left->right) leftNum = root->left->val;
+////        int rightNum = sumOfLeftLeaves(root->right);//右
+////        return leftNum + rightNum;//中
+////    }
+////
+////};
+////迭代法
+//class Solution {
+//public:
+//    int sumOfLeftLeaves(TreeNode* root) {
+//        stack<TreeNode*> st;
+//        if (root == NULL)return 0;
+//        st.push(root);
+//        int res = 0;
+//        while (!st.empty()) {
+//            TreeNode* node = st.top();
+//            st.pop();
+//            if (node->left != NULL && node->left->left == NULL && node->left->right == NULL) res += node->left->val;
+//            if (node->right) st.push(node->right);
+//            if (node->left) st.push(node->left);
+//        }
+//        return res;
+//    }
+//};
+//int main() {
+//    Solution s;
+//    TreeNode* root = new TreeNode(1);
+//    root->left = new TreeNode(2);
+//    root->right = new TreeNode(3);          
+//     
+//    root->left->left = new TreeNode(4);     
+//    root->left->right = new TreeNode(5);    
+//     
+//    root->right->left = new TreeNode(6);    
+//    root->right->right = new TreeNode(7);   
+//     
+//   
+//    return 0;
+//}           
---
+++ 路径总和.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+////递归法：
+//class Solution1 {
+//private:
+//    bool traversal(TreeNode* cur, int count) {
+//        if (cur->left == NULL && cur->right == NULL && count == 0) return true;
+//        if (!cur->left && !cur->right)return false;
+//
+//        if (cur->left) {
+//            count -= cur->left->val;
+//            if (traversal(cur->left, count)) return true;
+//            count += cur->left->val;
+//        }
+//        if (cur->right) {
+//            count -= cur->right->val;
+//            if (traversal(cur->right, count)) return true;
+//            count += cur->right->val;
+//        }
+//        return false;
+//    }
+//public:
+//    bool hasPathSum(TreeNode* root, int targetSum) {
+//        if (root == NULL) return false;
+//        return traversal(root, targetSum - root->val);
+//    }
+//};
+
+//迭代法：
+class Solution1 {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL) return false;
+        stack<pair<TreeNode*, int>> st;
+        st.push(pair<TreeNode*, int>(root, root->val));
+        while (!st.empty()) {
+            pair<TreeNode*, int>node = st.top();
+            st.pop();
+            if (!node.first->left && !node.first->right && targetSum == node.second)return true;
+
+            if (node.first->right) {
+                st.push(pair<TreeNode*, int>(node.first->right, node.second + node.first->right->val));
+            }
+            if (node.first->left) {
+                st.push(pair<TreeNode*, int>(node.first->left, node.second + node.first->left->val));
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+//路径总和2
+//给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+//说明:叶子节点是指没有子节点的节点。
+
+
+int main() {
+    Solution1 sol;
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);                
+    root->left-> right= new TreeNode(7);
+    root->right->left = new TreeNode(13);   
+    root->right->right = new TreeNode(4);
+    root->right->right->left = new TreeNode(5);
+
+    cout << sol.hasPathSum(root, 22) << endl;
+   
+    return 0;
+}       
